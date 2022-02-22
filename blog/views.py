08370f4153e0abe84ee9c1f2d2c948a8d3d41709
أحمد سلام <ahmed0salam22@gmail.com>
2022-02-22 15:19:13 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
+from django.views.decorators.vary import vary_on_cookie
 import logging
+
 
 logger = logging.getLogger(__name__)
 
