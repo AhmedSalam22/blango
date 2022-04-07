4e82ab7abd3e61555aa927c1d1667adfc47fe1ec
أحمد سلام <ahmed0salam22@gmail.com>
2022-04-07 21:19:32 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,10 +1,5 @@
-const theNumber = 1;
-let yourName = "Ahmed";
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-if (theNumber === 1 ) {
-  let yourName = "local Scope";
-  alert(yourName);
-
-}
-
-alert(yourName);
+numbers.forEach((value)=>{
+  console.log(value);
+});
