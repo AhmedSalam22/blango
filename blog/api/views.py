8b80da9cb99ae6c702ec6c768b5e6b89744d287f
أحمد سلام <ahmed0salam22@gmail.com>
2022-04-07 09:15:52 +0000

    Finish pagination and django filter

--- blog/api/views.py
+++ blog/api/views.py
@@ -19,7 +19,7 @@ from django.db.models import Q
 from django.utils import timezone
 from datetime import timedelta
 from django.http import Http404
-
+from blog.api.filters import PostFilterSet
 
 class TagViewSet(viewsets.ModelViewSet):
   queryset = Tag.objects.all()
@@ -29,8 +29,14 @@ class TagViewSet(viewsets.ModelViewSet):
   @action(methods=["get"], detail=True, name="Posts with the tag")
   def posts(self, request, pk=None):
     tag = self.get_object()
+    page = self.paginate_queryset(tag.posts)
+    if page is not None:
+        post_serializer = PostSerializer(
+            page, many=True, context={"request": request}
+        )
+        return self.get_paginated_response(post_serializer.data)
     post_serializer = PostSerializer(
-      tag.posts, many=True, context={"request": request}
+        tag.posts, many=True, context={"request": request}
     )
     return Response(post_serializer.data)
     
@@ -46,7 +52,10 @@ class TagViewSet(viewsets.ModelViewSet):
 
 class PostViewSet(viewsets.ModelViewSet):
   permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+#   filterset_fields = ["author", "tags"]
+  filterset_class = PostFilterSet
   queryset = Post.objects.all()
+  ordering_fields = ["published_at", "author", "title", "slug"]
 
   def get_serializer_class(self):
     if self.action in ("list", "create"):
@@ -94,11 +103,18 @@ class PostViewSet(viewsets.ModelViewSet):
   @method_decorator(vary_on_cookie)
   @action(methods=["get"], detail=False, name="Posts by the logged in user")
   def mine(self, request):
-      if request.user.is_anonymous:
-          raise PermissionDenied("You must be logged in to see which Posts are yours")
-      posts = self.get_queryset().filter(author=request.user)
-      serializer = PostSerializer(posts, many=True, context={"request": request})
-      return Response(serializer.data)
+    if request.user.is_anonymous:
+        raise PermissionDenied("You must be logged in to see which Posts are yours")
+    posts = self.get_queryset().filter(author=request.user)
+
+    page = self.paginate_queryset(posts)
+
+    if page is not None:
+        serializer = PostSerializer(page, many=True, context={"request": request})
+        return self.get_paginated_response(serializer.data)
+
+    serializer = PostSerializer(posts, many=True, context={"request": request})
+    return Response(serializer.data)
 
   @method_decorator(cache_page(120))
   @method_decorator(vary_on_headers("Authorization", "Cookie"))
